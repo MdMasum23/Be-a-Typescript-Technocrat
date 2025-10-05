e696543e004059785b268db83c9a9c09cc44541e
MdMasum23 <mdmasum67172019@gmail.com>
2025-10-05 19:27:24 +0600

    first code of typescript

---
+++ module_1/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let course = "next level web development";
+console.log(course);
+//# sourceMappingURL=index.js.map
---
+++ module_1/dist/mytest.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("once again test");
+//# sourceMappingURL=mytest.js.map
